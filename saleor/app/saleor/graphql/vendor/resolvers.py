7f3f5aa09dd5596ac856d99b826d70ae0c6acc06
Hallmann <dawid.hallmann@limebrains.com>
2021-01-22 15:09:25 +0100

    returning none if vendor not present in database

--- saleor/app/saleor/graphql/vendor/resolvers.py
+++ saleor/app/saleor/graphql/vendor/resolvers.py
@@ -10,5 +10,10 @@ def resolve_vendors(info, created, status, query, sort_by=None):
 
 def resolve_vendor(info, vendor_id, vendor_slug=None):
     if vendor_slug:
-        return models.Vendor.objects.get(slug=vendor_slug)
+        try:
+            vendor_model = models.Vendor.objects.get(slug=vendor_slug)
+        except models.Vendor.DoesNotExist as e:
+            print(f"---\nerror in resolve_vendor:\n{e}\n---")
+            vendor_model = None
+        return vendor_model
     return graphene.Node.get_node_from_global_id(info, vendor_id, Vendor)
